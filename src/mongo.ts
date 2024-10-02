eda3f5750413ed23e71477ae38f008882f01ec9d
Shahil Ahamad <shahilahamad04@gmail.com>
2024-10-02 11:48:49 +0545

    initial commit

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 async function run() {
-  const client = new MongoClient("mongodb://localhost:27017");
+  const client = new MongoClient("mongodb://admin:password@localhost:27018");
   try {
     await client.connect();
     console.log("Connected to MongoDB");
@@ -14,6 +14,33 @@ async function run() {
     // Query the document
     const user = await collection.findOne({ name: "Ram" });
     console.log("Found User:", user);
+
+    // CRUD
+    await collection.insertOne({ name: "Shyam", age: 30 });
+    const users = await collection.find();
+    console.log("users", users);
+
+    // Update a document
+    // discount all products
+    await collection.updateOne(
+      {
+        name: "Shyam",
+      },
+      {
+        $set: {
+          age: 25,
+          name: "Shyam Gupta",
+        },
+      }
+    );
+
+    const updatedUsers = await collection.find();
+    console.log("update users", updatedUsers);
+
+    // Delete a document
+    await collection.deleteOne({ name: "Shyam Gupta" });
+    const deletedUsers = await collection.find();
+    console.log("deleted users", deletedUsers);
   } finally {
     await client.close();
   }
